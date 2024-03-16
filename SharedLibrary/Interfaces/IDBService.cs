5f43a4c412ae1a41d069385d56952cf75ecda89c
Plexukas <algirdasleonardas@gmail.com>
2024-03-16 17:07:54 +0200

    Updated DBService interface

--- SharedLibrary/Interfaces/IDBService.cs
+++ SharedLibrary/Interfaces/IDBService.cs
@@ -2,8 +2,7 @@ namespace SharedLibrary.Interfaces
 {
     public interface IDBService
     {
-        Task<DbConnection> CreateConnection();
-        Task<T> GetAllAsync<T>();
+        Task<List<T>> GetAllAsync<T>();
         Task<T> GetByIdAsync<T>(int id);
         Task<T> CreateAsync<T>(T entity);
         Task<T> UpdateAsync<T>(T entity);
