1fa307d2910aee4ffd7d79b22a37a01116534cd9
Plexukas <algirdasleonardas@gmail.com>
2024-03-16 17:08:07 +0200

    Created DB Connection factory

---
+++ SharedLibrary/Services/DBConnectionFactory.cs
@@ -0,0 +1,20 @@
+namespace SharedLibrary.Services
+{
+    public class DBConnectionFactory 
+    {
+        
+        private readonly string _connectionString;
+
+        public DBConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<DbConnection> CreateConnectionAsync()
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
