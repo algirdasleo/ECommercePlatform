1b4de6deaf47789543018dc5f1f2525d6fbbc62c
Plexukas <algirdasleonardas@gmail.com>
2024-03-18 21:52:56 +0200

    added missing namespace

--- InventoryService/Services/InventoryDBService.cs
+++ InventoryService/Services/InventoryDBService.cs
@@ -3,76 +3,79 @@ using SharedLibrary.Services;
 using InventoryService.Models;
 using Dapper;
 
-public class InventoryDBService : IDBService<InventoryItem>
+namespace InventoryService.Services
 {
-    private readonly DBConnectionFactory _dbConnectionFactory;
-
-    public InventoryDBService(DBConnectionFactory dbConnectionFactory)
+    public class InventoryDBService : IDBService<InventoryItem>
     {
-        _dbConnectionFactory = dbConnectionFactory;
-    }
+        private readonly DBConnectionFactory _dbConnectionFactory;
 
-    public async Task<List<InventoryItem>> GetAllAsync()
-    {
-        using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+        public InventoryDBService(DBConnectionFactory dbConnectionFactory)
         {
-            var inventoryItems = await connection.QueryAsync<InventoryItem>("SELECT * FROM inventoryItems");
-            return inventoryItems.ToList();
+            _dbConnectionFactory = dbConnectionFactory;
         }
-    }
 
-    public async Task<InventoryItem?> GetByIdAsync(int id)
-    {
-        using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+        public async Task<List<InventoryItem>> GetAllAsync()
         {
-            var inventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>("SELECT * FROM inventoryItems WHERE inventoryItemId = @id", new { id });
-            return inventoryItem;
+            using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+            {
+                var inventoryItems = await connection.QueryAsync<InventoryItem>("SELECT * FROM inventoryItems");
+                return inventoryItems.ToList();
+            }
         }
-    }
 
-    public async Task<InventoryItem> CreateAsync(InventoryItem inventoryItem)
-    {
-        using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+        public async Task<InventoryItem?> GetByIdAsync(int id)
         {
-            var sqlQuery = @"
-                INSERT INTO inventoryItems (InventoryItemId, ProductId, QuantityAvailable)
-                VALUES (@InventoryItemId, @ProductId, @QuantityAvailable)
-                RETURNING *";
-            var createdInventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>(sqlQuery, inventoryItem);
-            if (createdInventoryItem == null)
-                throw new Exception("InventoryItem not created");
-            return createdInventoryItem;
+            using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+            {
+                var inventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>("SELECT * FROM inventoryItems WHERE inventoryItemId = @id", new { id });
+                return inventoryItem;
+            }
         }
-    }
 
-    public async Task<InventoryItem> UpdateAsync(InventoryItem inventoryItem)
-    {
-        using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+        public async Task<InventoryItem> CreateAsync(InventoryItem inventoryItem)
         {
-            var sqlQuery = @"
-                UPDATE inventoryItems
-                SET InventoryItemId = @InventoryItemId, ProductId = @ProductId,
-                    QuantityAvailable = @QuantityAvailable
-                RETURNING *";
-            var updatedInventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>(sqlQuery, inventoryItem);
-            if (updatedInventoryItem == null)
-                throw new Exception("InventoryItem not updated");
-            return updatedInventoryItem;
+            using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+            {
+                var sqlQuery = @"
+                    INSERT INTO inventoryItems (InventoryItemId, ProductId, QuantityAvailable)
+                    VALUES (@InventoryItemId, @ProductId, @QuantityAvailable)
+                    RETURNING *";
+                var createdInventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>(sqlQuery, inventoryItem);
+                if (createdInventoryItem == null)
+                    throw new Exception("InventoryItem not created");
+                return createdInventoryItem;
+            }
         }
-    }
 
-    public async Task<InventoryItem> DeleteAsync(int id)
-    {
-        using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+        public async Task<InventoryItem> UpdateAsync(InventoryItem inventoryItem)
         {
-            var sqlQuery = @"
-                DELETE FROM inventoryItems
-                WHERE inventoryItemId = @id
-                RETURNING *";
-            var deletedInventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>(sqlQuery, new { id });
-            if (deletedInventoryItem == null)
-                throw new Exception("InventoryItem not deleted");
-            return deletedInventoryItem;
+            using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+            {
+                var sqlQuery = @"
+                    UPDATE inventoryItems
+                    SET InventoryItemId = @InventoryItemId, ProductId = @ProductId,
+                        QuantityAvailable = @QuantityAvailable
+                    RETURNING *";
+                var updatedInventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>(sqlQuery, inventoryItem);
+                if (updatedInventoryItem == null)
+                    throw new Exception("InventoryItem not updated");
+                return updatedInventoryItem;
+            }
+        }
+
+        public async Task<InventoryItem> DeleteAsync(int id)
+        {
+            using (var connection = await _dbConnectionFactory.CreateConnectionAsync())
+            {
+                var sqlQuery = @"
+                    DELETE FROM inventoryItems
+                    WHERE inventoryItemId = @id
+                    RETURNING *";
+                var deletedInventoryItem = await connection.QueryFirstOrDefaultAsync<InventoryItem>(sqlQuery, new { id });
+                if (deletedInventoryItem == null)
+                    throw new Exception("InventoryItem not deleted");
+                return deletedInventoryItem;
+            }
         }
     }
 }
